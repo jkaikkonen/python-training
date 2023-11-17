67a4a859bdd642c5bba6f46df706a8d9a2215675
Jani Kaikkonen <jh.kaikkonen@gmail.com>
2023-11-17 15:34:25 +0200

    Amount of players and points done

--- pig.py
+++ pig.py
@@ -7,5 +7,42 @@ def roll():
     
     return roll
 
-value = roll()
-print(value)
+
+while True:
+    players = input("Enter the number of players (2-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else: 
+               print ("Must be between 2-4 players")
+    else:
+        print("Invalid input")
+        
+max_score = 50
+players_scores = [0 for _ in range(players)]
+
+while max(players_scores) < max_score:
+    
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx + 1, "turn has just started!\n")
+        current_score = 0
+        
+        while True: 
+            should_roll = input("Whould you like to roll (y)")
+            if should_roll.lower != "y":
+                break
+        
+        value = roll()
+        if value == 1:
+            print("You rolled a 1! Turn Done! ")
+            current_score = 0
+            break 
+        else: 
+            current_score += value
+            print ("You rolled a:," value )
+            
+    print("Your score is:", current_score)
+
+    players_scores[player_idx += current_score]
+    print("Your total score is:", players_scores[player_idx] )
